b5863ef7cae9edd5936a4d9397185a2aabe31f91
Aigul Biktasheva <meleuz13@gmail.com>
2021-04-03 00:30:49 +0500

    uptade commit

--- src/main/java/domain/LikesInfo.java
+++ src/main/java/domain/LikesInfo.java
@@ -1,30 +1,34 @@
 package domain;
-
 public class LikesInfo {
     private int count; /*number of users who liked the note*/
     private boolean userLikes; /*true, if the current user likes the note*/
+    private boolean canLike; /*true, if the current user may Like the note*/
     private boolean canRepost; /*true, if the current user may repost the note*/
 
     public int getCount() {
         return count;
     }
-
     public void setCount(int count) {
         this.count = count;
     }
-
     public boolean isUserLikes() {
         return userLikes;
     }
-
     public void setUserLikes(boolean userLikes) {
         this.userLikes = userLikes;
+    }
+
+    public boolean isCanLike() {
+        return canLike;
+    }
+
+    public void setCanLike(boolean canLike) {
+        this.canLike = canLike;
     }
 
     public boolean isCanRepost() {
         return canRepost;
     }
-
     public void setCanRepost(boolean canRepost) {
         this.canRepost = canRepost;
     }
